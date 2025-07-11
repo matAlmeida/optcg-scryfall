3f3e42b67ed44e7d2378917731a634e1b30f3223
Matheus Almeida Santos Anjos <mat.almeida@live.com>
2025-07-11 15:07:39 -0300

    chore: update parse path

--- src/scripts/parse_collections.js
+++ src/scripts/parse_collections.js
@@ -2,7 +2,7 @@ const { exec } = require('node:child_process');
 const list = require('./card-collections')
 
 list.forEach(async collection => {
-  exec(`python3 fetch_collection.py ./assets/collections/${collection.code}.html ./assets/parsed/${collection.code}`, (error, stdout, stderr) => {
+  exec(`python3 ./src/scripts/fetch_collection.py ./assets/collections/${collection.code}.html ./assets/parsed/${collection.code}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
