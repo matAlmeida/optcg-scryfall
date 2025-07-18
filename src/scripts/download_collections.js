960a0bd8d1bcbca57ea2a852e045291f8c9d0064
Matheus Almeida Santos Anjos <mat.almeida@live.com>
2025-07-18 17:02:55 -0300

    feat: add file check

--- src/scripts/download_collections.js
+++ src/scripts/download_collections.js
@@ -1,8 +1,14 @@
 const { exec } = require('node:child_process');
+const fs = require('fs');
 const list = require('./card-collections')
+const ASSETS_FOLDER = './assets/collections'
 
 list.forEach(async collection => {
-  exec(`wget https://en.onepiece-cardgame.com/cardlist/\?series\=${collection.series} -O ./assets/collections/${collection.code}.html`, (error, stdout, stderr) => {
+  const fileName = `${ASSETS_FOLDER}/${collection.code}.html`
+  if (fs.existsSync(fileName)) {
+    return
+  }
+  exec(`wget https://en.onepiece-cardgame.com/cardlist/\?series\=${collection.series} -O ${fileName}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
